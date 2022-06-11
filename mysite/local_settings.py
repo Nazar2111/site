4d1fdb30955ad3bd3817527a8786cd5452d7dc34
nazar <nazarov211190@gmail.com>
2022-06-11 18:31:12 +0300

    start project

---
+++ mysite/local_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-qsg8bj3*f94xr$c7@jg)dqvkt1zmse5ijlp3cm!n=z)!9&02kp'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite',
+        'USER': 'mysite',
+        'PASSWORD': 'mysite',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+DEBUG =True
