17aaba76e629d12ccb9384419b86e5a9300b2a90
nazar <nazarov211190@gmail.com>
2022-06-13 21:30:08 +0300

    add paginations, chenge views

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -140,5 +141,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+}
 
 from .local_settings import*
